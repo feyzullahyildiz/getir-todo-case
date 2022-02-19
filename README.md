bf81af763c93ea142cf578f4a9df5356aa169115
Feyzullah YILDIZ <ahmet.feyzullah.yildiz@gmail.com>
2022-02-20 00:22:38 +0300

    README.md updated. nodemon and ts-node is added to backend. yarn dev is working in the root level.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Development
+
+This project is started with lerna and you need to use yarn instead of npm.
+
+-   You need to run `yarn install` to install dependencies
+-   You need to run `yarn dev` to start frontend and backend in development mode.
--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,6 +1,15 @@
 import express from 'express';
 export const app = express();
 
+app.use((req, res, next) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    if (req.method === 'OPTIONS') {
+        return res.json({});
+    }
+    next();
+});
 app.get('/data', (req, res) => {
     res.json({ foo: 'bar' });
 });
